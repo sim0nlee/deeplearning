0af4147aa3efcb98e171cbb04b3bdd4032e6d9b8
sim0nlee <simonelibutti@hotmail.com>
2024-01-07 10:52:27 +0100

    Added convolution gradients to the print in train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 
 from activation import TReLU
+from models import HiddenBlockCNN
 
 
 def train(dataloader, batch_size, model, loss_fn, optimizer, epoch, device, writer=None):
@@ -38,18 +39,37 @@ def print_model_data(model, n_steps, cur_epoch, cur_iter, writer):
         if isinstance(module, torch.nn.Linear):
             weight_grads.append(module.weight.grad.ravel())
             bias_grads.append(module.bias.grad.ravel())
-        if isinstance(module, TReLU):
+        elif isinstance(module, TReLU):
             for param in module.parameters():
                 alphas.append(param.data.item())
+        elif isinstance(module, HiddenBlockCNN):
+            weight_grads.append(module.conv.weight.grad.ravel())
+            bias_grads.append(module.conv.bias.grad.ravel())
+            if isinstance(module.activation, TReLU):
+                for param in module.activation.parameters():
+                    alphas.append(param.data.item())
 
     weight_grad_norms = torch.linalg.norm(torch.cat(weight_grads))
     bias_grad_norms = torch.linalg.norm(torch.cat(bias_grads))
     print()
     print('Weight grads norm:', weight_grad_norms.item())
     print('Bias grads norm:', bias_grad_norms.item())
+
+    if model.beta is not None:
+        if model.beta_is_global:
+            print('Residual Connection Global Beta:', model.beta.item())
+        else:
+            print('Residual Connection Betas:', [b.item() for b in model.beta])
+
     if writer:
         writer.add_scalar('Weights Gradient Norms', weight_grad_norms.item(), n_steps * cur_epoch + cur_iter)
         writer.add_scalar('Bias Gradient Norms', bias_grad_norms.item(), n_steps * cur_epoch + cur_iter)
+        if model.beta is not None:
+            if model.beta_is_global:
+                writer.add_scalar('Global Beta', model.beta.item(), n_steps * cur_epoch + cur_iter)
+            else:
+                for i, b in enumerate(model.beta):
+                    writer.add_scalar(f'Betas/Beta_{i}', b.item(), n_steps * cur_epoch + cur_iter)
     print('TReLU alphas:', ["{:.5f}".format(alpha) for alpha in alphas])
     print()
 
