280f81f733a19c7452ce4f3ba4006ab6b855069f
sim0nlee <simonelibutti@hotmail.com>
2024-01-07 19:36:08 +0100

    updated mlp with residuals

--- models.py
+++ models.py
@@ -4,98 +4,306 @@ import numpy as np
 from activation import TReLU
 
 
+# class MNIST_MLP(torch.nn.Module):
+#     def __init__(self,
+#                  device,
+#                  depth,
+#                  width,
+#                  activation,
+#                  alpha_init=1.0,
+#                  trelu_is_trainable=False,
+#                  normalize=False):
+#         super().__init__()
+#
+#         if activation not in ["relu", "lrelu", "trelu"]:
+#             raise Exception("Invalid Activation")
+#
+#         self.device = device
+#         self.depth = depth
+#         self.width = width
+#         self.activation = activation
+#         self.alpha_init = alpha_init
+#         self.trelu_is_trainable = trelu_is_trainable
+#         self.normalize = normalize
+#
+#         layers = []
+#
+#         for d in range(depth):
+#             # Input layer
+#             if d == 0:
+#                 layers.append(torch.nn.Linear(28 * 28, width))
+#             # Last layer
+#             elif d == depth - 1:
+#                 layers.append(torch.nn.Linear(width, 10))
+#             # Hidden layers
+#             else:
+#                 layer = torch.nn.Linear(width, width)
+#                 # Custom weight and bias initialization
+#                 torch.nn.init.normal_(layer.weight, 0, 1 / width)
+#                 torch.nn.init.constant_(layer.bias, 0)
+#                 layers.append(torch.nn.Linear(width, width))
+#                 if normalize:
+#                     layers.append(torch.nn.BatchNorm1d(width))
+#             # Activation functions after all layers but the last
+#             if d < depth - 1:
+#                 if activation == "relu":
+#                     layers.append(torch.nn.ReLU())
+#                 elif activation == "lrelu":
+#                     layers.append(torch.nn.LeakyReLU())
+#                 elif activation == "trelu":
+#                     layers.append(TReLU(alpha_init, trainable=trelu_is_trainable, device=self.device))
+#
+#         self.net = torch.nn.Sequential(*layers)
+#
+#         self.print_model_info()
+#
+#     def base_params(self):
+#         """Returns a list of the linear layer and batch normalization layer parameters"""
+#         params = []
+#         for layer in self.net:
+#             if isinstance(layer, torch.nn.Linear) or isinstance(layer, torch.nn.BatchNorm1d):
+#                 for param in layer.parameters():
+#                     params.append(param)
+#         return params
+#
+#     def trelu_params(self):
+#         """Returns a list of the alpha parameters of each TReLU layer of the network, if present"""
+#         params = []
+#         for layer in self.net:
+#             if isinstance(layer, TReLU):
+#                 for param in layer.parameters():
+#                     params.append(param)
+#         return params
+#
+#     def forward(self, x, shallow=False):
+#         x = torch.nn.Flatten()(x)
+#         for module in self.net:
+#             x = x.type(torch.float32)
+#             x = module(x)
+#         return x
+#
+#     def print_model_info(self):
+#         print(f"Network Depth: {self.depth}, Network Width: {self.width}.")
+#         print(f"Using {self.activation} activation")
+#         if self.activation == "trelu":
+#             print(f"TReLU alpha initialization: {self.alpha_init}.")
+#             if self.trelu_is_trainable:
+#                 print("TReLU is trainable.")
+#             else:
+#                 print("TReLU is not trainable")
+#         if self.normalize:
+#             print("Batch normalization on")
+#         else:
+#             print("Batch normalization off")
+#         print()
+
+
+class HiddenBlock(torch.nn.Module):
+    def __init__(self,
+                 width,
+                 activation_module,
+                 residual=True,
+                 activation_before_residual=False,
+                 normalize=False):
+        super().__init__()
+
+        self.residual = residual
+        self.activation_before_residual = activation_before_residual
+
+        self.linear = torch.nn.Linear(width, width)
+        self.activation = activation_module
+        self.bn = None
+        if normalize:
+            self.bn = torch.nn.BatchNorm1d(width)
+
+    def base_params(self):
+        params = []
+        net = [self.linear, self.bn] if self.bn is not None else [self.linear]
+        for l in net:
+            for param in l.parameters():
+                params.append(param)
+        return params
+
+    def trelu_params(self):
+        params = []
+        if isinstance(self.activation, TReLU):
+            for param in self.activation.parameters():
+                params.append(param)
+        return params
+
+    def forward(self, x, beta=1.0, depth_scaler=1.0):  # beta and depth_scaler are only used for residual connections
+        if self.residual:
+            if self.activation_before_residual:
+                x_aux = beta * self.linear(x) / depth_scaler
+                if self.bn is not None:
+                    x_aux = self.bn(x_aux)
+                x_aux = self.activation(x_aux)
+                x = x + x_aux
+            else:
+                x = x + beta * self.linear(x) / depth_scaler
+                if self.bn is not None:
+                    x = self.bn(x)
+                x = self.activation(x)
+
+        else:
+            x = self.linear(x)
+            if self.bn is not None:
+                x = self.bn(x)
+            x = self.activation(x)
+
+        return x
+
+
 class MNIST_MLP(torch.nn.Module):
     def __init__(self,
-                 device,
                  depth,
                  width,
                  activation,
                  alpha_init=1.0,
+                 device="cpu",
                  trelu_is_trainable=False,
+                 residual_connections=False,
+                 beta_init=1.0,
+                 beta_is_trainable=False,
+                 beta_is_global=True,
+                 activation_before_residual=False,
                  normalize=False):
         super().__init__()
 
-        if activation not in ["relu", "lrelu", "trelu"]:
-            raise Exception("Invalid Activation")
+        print(f"Network Depth: {depth}, Network Width: {width}.")
+        print(f"Using {activation} activation.")
+        if activation == "trelu":
+            print(f"TReLU alpha initialization: {alpha_init}.")
+            if trelu_is_trainable:
+                print("TReLU is trainable.")
+        if residual_connections:
+            print("Residual connections on.")
+            print(f"Residual connections beta initialization: {beta_init}.")
+            if beta_is_trainable:
+                print("Beta is trainable.")
+                if beta_is_global:
+                    print("Beta is global.")
+                else:
+                    print("Beta is per layer.")
+        if normalize:
+            print("Batch normalization on.")
+        print()
 
-        self.device = device
         self.depth = depth
         self.width = width
         self.activation = activation
+        self.device = device
         self.alpha_init = alpha_init
+        self.beta_init = beta_init
         self.trelu_is_trainable = trelu_is_trainable
+        self.beta_is_global = beta_is_global
+        self.beta_is_trainable = beta_is_trainable
+        self.residual_connections = residual_connections
+        self.activation_before_residual = activation_before_residual
         self.normalize = normalize
 
         layers = []
 
-        for d in range(depth):
-            # Input layer
-            if d == 0:
-                layers.append(torch.nn.Linear(28 * 28, width))
-            # Last layer
-            elif d == depth - 1:
-                layers.append(torch.nn.Linear(width, 10))
-            # Hidden layers
+        self.beta = None
+        if residual_connections:
+            if beta_is_global:
+                self.beta = torch.nn.Parameter(torch.tensor(beta_init), requires_grad=beta_is_trainable)
             else:
-                layer = torch.nn.Linear(width, width)
-                # Custom weight and bias initialization
-                torch.nn.init.normal_(layer.weight, 0, 1 / width)
-                torch.nn.init.constant_(layer.bias, 0)
-                layers.append(torch.nn.Linear(width, width))
-                if normalize:
-                    layers.append(torch.nn.BatchNorm1d(width))
-            # Activation functions after all layers but the last
-            if d < depth - 1:
-                if activation == "relu":
+                self.beta = torch.nn.ParameterList(
+                    [torch.nn.Parameter(torch.tensor(beta_init), requires_grad=beta_is_trainable) for _ in range(depth - 2)])
+
+        for d in range(depth):
+            if d == 0:  # Input layer
+                layers.append(torch.nn.Linear(28 * 28, width))
+                # layers.append(nn.Linear(12288, width))
+                if activation == "relu":  # Activation functions after all layers but the last
                     layers.append(torch.nn.ReLU())
-                elif activation == "lrelu":
-                    layers.append(torch.nn.LeakyReLU())
                 elif activation == "trelu":
                     layers.append(TReLU(alpha_init, trainable=trelu_is_trainable, device=self.device))
 
-        self.net = torch.nn.Sequential(*layers)
+            elif d == depth - 1:  # Last layer
+                layers.append(torch.nn.Linear(width, 10))
+                # layers.append(nn.Linear(width, 200))
 
-        self.print_model_info()
+            else:  # Hidden layers
+                activation_module = torch.nn.ReLU() if activation == "relu" else TReLU(alpha_init,
+                                                                                       trainable=trelu_is_trainable,
+                                                                                       device=self.device)
+                layers.append(HiddenBlock(width,
+                                          activation_module,
+                                          residual=residual_connections,
+                                          activation_before_residual=activation_before_residual,
+                                          normalize=normalize))
+
+        self.net = torch.nn.ModuleList(layers)
 
     def base_params(self):
-        """Returns a list of the linear layer and batch normalization layer parameters"""
         params = []
-        for layer in self.net:
-            if isinstance(layer, torch.nn.Linear) or isinstance(layer, torch.nn.BatchNorm1d):
-                for param in layer.parameters():
+        for l in self.net:
+            if isinstance(l, HiddenBlock):
+                params += l.base_params()
+            elif isinstance(l, torch.nn.Linear) or isinstance(l, torch.nn.BatchNorm1d):
+                for param in l.parameters():
                     params.append(param)
         return params
 
     def trelu_params(self):
-        """Returns a list of the alpha parameters of each TReLU layer of the network, if present"""
         params = []
-        for layer in self.net:
-            if isinstance(layer, TReLU):
-                for param in layer.parameters():
+        for l in self.net:
+            if isinstance(l, HiddenBlock):
+                params += l.trelu_params()
+            elif isinstance(l, TReLU):
+                for param in l.parameters():
                     params.append(param)
         return params
 
     def forward(self, x, shallow=False):
         x = torch.nn.Flatten()(x)
+
+        beta_idx = 0
+
+        if self.residual_connections and not self.beta_is_global and shallow:
+            betas_tensor = torch.cat([b.view(-1) for b in self.beta])
+            beta_threshold = 0.1 * torch.max(betas_tensor)
+            n_skipped_layers = torch.sum(betas_tensor <= beta_threshold)
+            print(f"Shallow mode: Skipping {n_skipped_layers} layers with beta <= {beta_threshold}.")
+
         for module in self.net:
-            x = x.type(torch.float32)
-            x = module(x)
+            if self.residual_connections and isinstance(module, HiddenBlock):
+                beta = self.beta if self.beta_is_global else self.beta[beta_idx]
+                beta_idx += 1
+                if not shallow or beta > beta_threshold:
+                    x = module(x, beta=beta, depth_scaler=np.sqrt(self.depth))
+
+            else:
+                x = module(x)
+
         return x
 
     def print_model_info(self):
-        print(f"Network Depth: {self.depth}, Network Width: {self.width}.")
-        print(f"Using {self.activation} activation")
+        print(f"Network Depth: {self.depth}, Kernels: {self.kernels}.")
+        print(f"Using {self.activation} activation.")
         if self.activation == "trelu":
             print(f"TReLU alpha initialization: {self.alpha_init}.")
             if self.trelu_is_trainable:
                 print("TReLU is trainable.")
-            else:
-                print("TReLU is not trainable")
+        if self.residual_connections:
+            print("Residual connections on.")
+            print(f"Residual connections beta initialization: {self.beta_init}.")
+            if self.beta_is_trainable:
+                print("Beta is trainable.")
+                if self.beta_is_global:
+                    print("Beta is global.")
+                else:
+                    print("Beta is per layer.")
         if self.normalize:
-            print("Batch normalization on")
-        else:
-            print("Batch normalization off")
+            print("Batch normalization on.")
         print()
+
+
+
+
+
 
 
 class HiddenBlockCNN(torch.nn.Module):
